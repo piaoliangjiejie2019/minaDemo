6d9d7b207fc36b103a644f1c54d440d84761c02b
piaoliangjiejie2019 <piaoliangjiejie2019@outlook.com>
2021-08-12 01:17:36 +0800

    8.12 1:17 finish swiper

--- component/Swiper/Swiper.js
+++ component/Swiper/Swiper.js
@@ -11,7 +11,21 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    swiperList:[
+      {
+        id:0,
+        imgsrc:"https://s3.bmp.ovh/imgs/2021/08/3ee283554cb39807.jpg"
+      },{
+        id:1,
+        imgsrc:"https://s3.bmp.ovh/imgs/2021/08/42371480753ed770.jpg"
+      }, {
+        id:2,
+        imgsrc:"https://s3.bmp.ovh/imgs/2021/08/4cffdf7a7b16dbdf.jpg"
+      }, {
+        id:3,
+        imgsrc:"https://s3.bmp.ovh/imgs/2021/08/5f86fefed6918ebd.jpg"
+      },
+    ]
   },
 
   /**
